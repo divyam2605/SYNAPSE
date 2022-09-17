049db863744bd5bccf896ab4ffb840482306e51d
divyam2605 <104719264+divyam2605@users.noreply.github.com>
2022-09-17 17:54:31 +0530

    Delete app2 directory

--- app2/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-import requests
-
-
-def countries_api(request):
-    response = requests.get("https://api.covid19api.com/countries").json()
-    return render(request, "index2.html", {"response": response})
